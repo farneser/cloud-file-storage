e725d539ecc823bff6466cc204813d844af9be81
farneser <farneser1@gmail.com>
2023-10-19 01:42:25 +0300

    refactor: no more useless code

--- run-dev.sh
+++ run-dev.sh
@@ -1,2 +1,2 @@
-./mvnw install
-sudo docker-compose -f docker/docker-compose-dev-local.yml up
+./mvnw package
+docker-compose -f docker/docker-compose-dev-local.yml up --build
--- src/main/java/com/farneser/cloudfilestorage/repository/SessionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.farneser.cloudfilestorage.repository;
-
-import com.farneser.cloudfilestorage.models.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<Session, String> {
-}
