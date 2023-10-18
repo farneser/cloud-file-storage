acc6ecf5245af4bc9b12bb5af1aa71bb1e71053d
farneser <farneser1@gmail.com>
2023-10-19 02:36:30 +0300

    update: user columns unique

--- data/init-scripts/000_ddl_create_users_table.sql
+++ data/init-scripts/000_ddl_create_users_table.sql
@@ -1,7 +1,7 @@
 create table users
 (
-    id       bigserial not null,
-    password varchar(255),
-    username varchar(255),
+    id       bigserial    not null unique,
+    username varchar(255) not null unique,
+    password varchar(255) not null,
     primary key (id)
 )
--- src/main/java/com/farneser/cloudfilestorage/models/User.java
+++ src/main/java/com/farneser/cloudfilestorage/models/User.java
@@ -9,9 +9,12 @@ import lombok.Data;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
 
+    @Column(nullable = false)
     private String password;
 }
