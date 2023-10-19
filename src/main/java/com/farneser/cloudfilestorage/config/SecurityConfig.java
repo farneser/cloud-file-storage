d36d80a21b098208d7037f54a191719f55e7b245
farneser <farneser1@gmail.com>
2023-10-19 03:28:26 +0300

    feat: security config authenticationManagerBean

--- src/main/java/com/farneser/cloudfilestorage/config/SecurityConfig.java
+++ src/main/java/com/farneser/cloudfilestorage/config/SecurityConfig.java
@@ -1,17 +1,30 @@
 package com.farneser.cloudfilestorage.config;
 
+import com.farneser.cloudfilestorage.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final UserService userService;
+    private final PasswordConfig passwordConfig;
+
+    @Autowired
+    public SecurityConfig(UserService userDetailsService, PasswordConfig passwordConfig) {
+        this.userService = userDetailsService;
+        this.passwordConfig = passwordConfig;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -21,13 +34,24 @@ public class SecurityConfig {
                 })
                 .formLogin(login -> {
                     login.loginPage("/login");
-                    login.defaultSuccessUrl("/");
+                    login.defaultSuccessUrl("/hello");
                     login.permitAll();
                 })
                 .logout(LogoutConfigurer::permitAll)
-                .httpBasic(HttpBasicConfigurer::notify)
+//                .httpBasic(HttpBasicConfigurer::notify)
                 .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(HttpSecurity http) throws Exception {
+        var authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder
+                .userDetailsService(userService)
+                .passwordEncoder(passwordConfig.passwordEncoder());
+
+        return authenticationManagerBuilder.build();
     }
 }
