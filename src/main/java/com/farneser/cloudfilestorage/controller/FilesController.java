571dcd7d38ce7b4232c3e749cf887643fc19836d
farneser <farneser1@gmail.com>
2023-10-25 00:39:50 +0300

    feat: files controller and template

---
+++ src/main/java/com/farneser/cloudfilestorage/controller/FilesController.java
@@ -0,0 +1,48 @@
+package com.farneser.cloudfilestorage.controller;
+
+import com.farneser.cloudfilestorage.service.StorageService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Slf4j
+@Controller
+@RequestMapping("/storage")
+public class FilesController {
+    private final StorageService minioService;
+
+    public FilesController(StorageService storageService) {
+        this.minioService = storageService;
+    }
+
+    @GetMapping("/files")
+    public String dsad() {
+        return "upload_form";
+    }
+
+    @PostMapping("/files")
+    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes, HttpSession session) {
+        minioService.uploadFile(getCurrentPath(session), file);
+        redirectAttributes.addFlashAttribute("message", "File uploaded successfully!");
+        return "redirect:/storage/files";
+    }
+
+    private static String getCurrentPath(HttpSession session) {
+        var path = session.getAttribute("path");
+
+        if (path == null) {
+            return "/";
+        }
+
+        return path.toString();
+    }
+
+}
