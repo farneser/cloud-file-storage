70dd97f7997b1e7a8aac9268a5286579f9500b63
farneser <farneser1@gmail.com>
2023-10-27 01:27:00 +0300

    feat: file controller download endpoint

--- src/main/java/com/farneser/cloudfilestorage/controller/FilesController.java
+++ src/main/java/com/farneser/cloudfilestorage/controller/FilesController.java
@@ -3,7 +3,11 @@ package com.farneser.cloudfilestorage.controller;
 import com.farneser.cloudfilestorage.exception.MinioException;
 import com.farneser.cloudfilestorage.service.StorageService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +22,22 @@ public class FilesController {
 
     public FilesController(StorageService storageService) {
         this.storageService = storageService;
+    }
+
+    @GetMapping
+    public ResponseEntity<InputStreamResource> download(@RequestParam("path") String path) {
+        try {
+            var fileDto = storageService.download(path);
+            var headers = new HttpHeaders();
+
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileDto.getFileName());
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(new InputStreamResource(fileDto.getFile()));
+        } catch (MinioException e) {
+            return ResponseEntity.status(500).body(null);
+        }
     }
 
     @PostMapping("/file")
