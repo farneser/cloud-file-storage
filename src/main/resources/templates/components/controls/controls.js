ea3853967767a2d4b33156eecb87df69f0ca3536
farneser <farneser1@gmail.com>
2023-10-26 10:52:40 +0300

    refactor: controls

---
+++ src/main/resources/templates/components/controls/controls.js
@@ -0,0 +1,26 @@
+const controlsPopup = document.getElementById("popup");
+
+document.getElementById("create-folder").addEventListener("click", () => openPopup("create-folder-form"));
+document.getElementById("post-file").addEventListener("click", () => openPopup("post-file-form"));
+document.getElementById("post-folder").addEventListener("click", () => openPopup("post-folder-form"));
+
+
+function openPopup(formId) {
+    const formContainers = document.querySelectorAll(".form-container");
+    formContainers.forEach((container) => {
+        container.style.display = container.id === formId ? "flex" : "none";
+    });
+
+    controlsPopup.style.display = "flex";
+}
+
+function closePopup() {
+    controlsPopup.style.display = "none";
+}
+
+
+controlsPopup.addEventListener("click", function (event) {
+    if (event.target === controlsPopup) {
+        closePopup();
+    }
+});
