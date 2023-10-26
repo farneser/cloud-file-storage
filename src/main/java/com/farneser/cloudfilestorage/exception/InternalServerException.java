033cde5e02863b68da17d1f153ef34b4554b2385
farneser <farneser1@gmail.com>
2023-10-26 11:30:42 +0300

    feat: internal server exception

---
+++ src/main/java/com/farneser/cloudfilestorage/exception/InternalServerException.java
@@ -0,0 +1,7 @@
+package com.farneser.cloudfilestorage.exception;
+
+public class InternalServerException extends Exception {
+    public InternalServerException(String message) {
+        super(message);
+    }
+}
