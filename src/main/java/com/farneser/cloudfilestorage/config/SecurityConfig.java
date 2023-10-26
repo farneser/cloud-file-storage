354aaf8248016ac6fc5a741bb745245aab4d85a8
farneser <farneser1@gmail.com>
2023-10-26 14:47:48 +0300

    refactor: thymeleaf fragments, security config, styles and scripts

--- src/main/java/com/farneser/cloudfilestorage/config/SecurityConfig.java
+++ src/main/java/com/farneser/cloudfilestorage/config/SecurityConfig.java
@@ -4,10 +4,13 @@ import com.farneser.cloudfilestorage.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,9 +33,12 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers("/register").permitAll();
+                    req.requestMatchers(HttpMethod.GET, "/styles/**", "/scripts/**").permitAll();
                     req.anyRequest().authenticated();
                 })
                 .formLogin(login -> {
+                    login.usernameParameter("username");
+                    login.passwordParameter("password");
                     login.loginPage("/login");
                     login.defaultSuccessUrl("/");
                     login.permitAll();
