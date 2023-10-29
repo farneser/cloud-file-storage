1e27a25400831c3a80361b07b16cd000d36c6580
farneser <farneser1@gmail.com>
2023-10-29 15:28:12 +0300

    feat: InputStreamUtils.compressToZip method

---
+++ src/main/java/com/farneser/cloudfilestorage/utils/InputStreamUtils.java
@@ -0,0 +1,44 @@
+package com.farneser.cloudfilestorage.utils;
+
+import com.farneser.cloudfilestorage.dto.FileDto;
+import com.farneser.cloudfilestorage.exception.InternalServerException;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class InputStreamUtils {
+
+    public static InputStream compressToZip(List<FileDto> fileDtoList) throws IOException, InternalServerException {
+        if (fileDtoList == null || fileDtoList.isEmpty()) {
+            throw new InternalServerException("Files not found");
+        }
+
+        var byteArrayOutputStream = new ByteArrayOutputStream();
+
+        var zipOutputStream = new ZipOutputStream(byteArrayOutputStream);
+
+        for (FileDto fileDto : fileDtoList) {
+            var entryPath = Paths.get(fileDto.getPath(), fileDto.getFileName()).toString();
+
+            ZipEntry zipEntry = new ZipEntry(entryPath);
+            zipOutputStream.putNextEntry(zipEntry);
+
+            var buffer = new byte[1024];
+            var len = 0;
+            var inputStream = fileDto.getFile();
+
+            while ((len = inputStream.read(buffer)) > 0) {
+                zipOutputStream.write(buffer, 0, len);
+            }
+
+            zipOutputStream.closeEntry();
+        }
+
+        zipOutputStream.close();
+
+        return new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+    }
+}
